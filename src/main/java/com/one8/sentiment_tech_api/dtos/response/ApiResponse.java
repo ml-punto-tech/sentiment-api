445fca873a26b001c3e82c1d149c09a9952be0a2
WilmarDeML <carewaz23@hotmail.com>
2025-12-22 00:44:41 -0500

    Agrega respuesta genérica(dto) de la api

---
+++ src/main/java/com/one8/sentiment_tech_api/dtos/response/ApiResponse.java
@@ -0,0 +1,21 @@
+package com.one8.sentiment_tech_api.dtos.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Respuesta genérica de la API.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    private boolean success;
+    private T data;
+    private String message;
+}
