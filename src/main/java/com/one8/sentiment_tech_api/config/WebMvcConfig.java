95e84e221ebf0812128a4e953eaab54e0e67d331
WilmarDeML <carewaz23@hotmail.com>
2025-12-29 19:31:08 -0500

    Add cors configuration

---
+++ src/main/java/com/one8/sentiment_tech_api/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.one8.sentiment_tech_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://ml-punto-tech.github.io/sentiment-api/",
+                        "https://sentimient-walo.vercel.app/"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+//                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
+
