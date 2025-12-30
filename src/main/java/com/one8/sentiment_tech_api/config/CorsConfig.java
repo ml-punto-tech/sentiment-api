1e581579ab43224f5a3d9ca2f10214fd2266bfa0
Adrián Luna <adrianluna012@gmail.com>
2025-12-29 19:04:49 -0600

    Cors config

---
+++ src/main/java/com/one8/sentiment_tech_api/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.one8.sentiment_tech_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:3000", "https://sentimient-walo.vercel.app/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
