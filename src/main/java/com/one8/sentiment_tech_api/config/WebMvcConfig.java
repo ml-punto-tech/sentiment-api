704f907d28aca094fe14c7d676d98b92e2bca511
Wilmar De MelquisedecLisbet <carewaz23@hotmail.com>
2025-12-29 19:56:09 -0500

    Update WebMvcConfig.java

--- src/main/java/com/one8/sentiment_tech_api/config/WebMvcConfig.java
+++ src/main/java/com/one8/sentiment_tech_api/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "https://ml-punto-tech.github.io/sentiment-api/",
+                        "https://ml-punto-tech.github.io/",
                         "https://sentimient-walo.vercel.app/"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
